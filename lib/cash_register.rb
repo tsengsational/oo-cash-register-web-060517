2a23a66b5123c773ab4f8fb071b3363d5d2c0c6e
github username <github email address>
2017-06-08 20:11:20 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,41 @@
+require 'pry-byebug'
 
+class CashRegister
+attr_accessor :total, :last_item
+attr_reader :discount, :items
+
+  def initialize(*discount)
+    @total = 0
+    @discount = discount.join.to_i if !discount.nil?
+    @items = []
+    @last_item = {}
+  end
+
+  def add_item(title, price, *quantity)
+    quantity.size > 0 ? quant = quantity.join.to_i : quant = 1
+    @total += (price * quant)
+    @last_item[:price] = price
+    @last_item[:title] = title
+    @last_item[:quantity] = quant
+    quant.times do
+      @items << title
+      # binding.pry
+    end
+  end
+
+  def apply_discount
+    if @discount > 0
+      (@total *= ((100.to_f - @discount.to_f)/100.to_f)).to_i
+      return "After the discount, the total comes to $#{@total.to_i}."
+    else
+      return "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    @total -= (@last_item[:price] * @last_item[:quantity])
+    @items.pop(@last_item[:quantity])
+  end
+end
+
+# Pry.start
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 describe 'CashRegister' do
   let(:cash_register) { CashRegister.new }
   let(:cash_register_with_discount) { CashRegister.new(20) }
